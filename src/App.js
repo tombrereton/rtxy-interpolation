5196aafcbc2a4b81d4122f8b4a1bb0812753f6f6
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2022-09-10 23:16:47 +0200

    Update website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [point1x, setPoint1x] = useState(0)
+  const [point1y, setPoint1y] = useState(0)
+  const [point2x, setPoint2x] = useState(0)
+  const [point2y, setPoint2y] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type="text" onChange={e => setPoint1x(e.target.value)}></input>
+        <input type="text" onChange={e => setPoint1y(e.target.value)}></input>
+        <input type="text" onChange={e => setPoint2x(e.target.value)}></input>
+        <input type="text" onChange={e => setPoint2y(e.target.value)}></input>
+        <p>{point1x}</p>
+        <p>{point1y}</p>
+        <p>{point2x}</p>
+        <p>{point2y}</p>
+        <p>{point1x * point1y}</p>
     </div>
   );
 }
